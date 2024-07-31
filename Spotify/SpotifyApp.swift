b203a0c267b10cf43f6e158c28b59b9e7ca84151
Feyzullah Kodat <feyzkodat@gmail.com>
2024-07-31 20:09:06 +0300

    lets get this over with

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Feyzullah Kodat on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
